8995c6c762c3a53ef410734dcfb76d72ef684d50
Avdhoot Patil <avdhoot.980@gmail.com>
2020-10-15 22:42:33 +0530

    Added orgDetails endpoints in organization master for getting organization details in form

--- org-master-mock-api/server.js
+++ org-master-mock-api/server.js
@@ -7,6 +7,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({ origin: true }));
 
+var getOrganisationData = require("./data/getOrganisationData");
+
+app.get("/organisation", function (req, res) {
+  console.log("/organisation");
+  res.status(200).send(getOrganisationData());
+});
+
 app.listen(6001, () => {
   console.log("Server started at 6001");
 });
