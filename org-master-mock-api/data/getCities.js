fd1c9b5df6b9d07af66ce11ba53d8ca1c555351a
Avdhoot Patil <avdhoot.980@gmail.com>
2020-10-20 11:47:54 +0530

    Added endpoints in organisation master

---
+++ org-master-mock-api/data/getCities.js
@@ -0,0 +1,20 @@
+module.exports = () => {
+  return [
+    {
+      id: 1,
+      name: "Pune",
+    },
+    {
+      id: 2,
+      name: "Mumbai",
+    },
+    {
+      id: 3,
+      name: "Delhi",
+    },
+    {
+      id: 4,
+      name: "Panaji",
+    },
+  ];
+};
--- org-master-mock-api/server.js
+++ org-master-mock-api/server.js
@@ -8,10 +8,28 @@ app.use(bodyParser.json());
 app.use(cors({ origin: true }));
 
 var getOrganisationData = require("./data/getOrganisationData");
+var getCountries = require("./data/getCountries");
+var getStates = require("./data/getStates");
+var getCities = require("./data/getCities");
 
 app.get("/organisation", function (req, res) {
   console.log("/organisation");
   res.status(200).send(getOrganisationData());
+});
+
+app.get("/countries", function (req, res) {
+  console.log("/countries");
+  res.status(200).send(getCountries());
+});
+
+app.get("/states", function (req, res) {
+  console.log("/states");
+  res.status(200).send(getStates());
+});
+
+app.get("/cities", function (req, res) {
+  console.log("/cities");
+  res.status(200).send(getCities());
 });
 
 app.listen(6001, () => {
