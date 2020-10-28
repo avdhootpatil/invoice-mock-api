4dfe206ad5101fa32a70305cbe856bde2c76f2db
Avdhoot Patil <avdhoot.980@gmail.com>
2020-10-28 11:16:12 +0530

    initial setup invoice list mock api.

---
+++ invoice_list_mock_api/data/getInvoiceList.js
@@ -0,0 +1,53 @@
+module.exports = () => {
+  return [
+    {
+      id: 1,
+      invoiceNo: "a123",
+      invoiceDate: new Date().toISOString(),
+      customerName: "ferrari",
+      Amount: 1000,
+    },
+    {
+      id: 2,
+      invoiceNo: "a234",
+      invoiceDate: new Date().toISOString(),
+      customerName: "Maclaren",
+      Amount: 198,
+    },
+    {
+      id: 3,
+      invoiceNo: "a345",
+      invoiceDate: new Date().toISOString(),
+      customerName: "Mercedes",
+      Amount: 100000,
+    },
+    {
+      id: 4,
+      invoiceNo: "a456",
+      invoiceDate: new Date().toISOString(),
+      customerName: "Renault",
+      Amount: 100,
+    },
+    {
+      id: 5,
+      invoiceNo: "a567",
+      invoiceDate: new Date().toISOString(),
+      customerName: "Hass",
+      Amount: 230,
+    },
+    {
+      id: 6,
+      invoiceNo: "a678",
+      invoiceDate: new Date().toISOString(),
+      customerName: "",
+      Amount: 433,
+    },
+    {
+      id: 7,
+      invoiceNo: "a789",
+      invoiceDate: new Date().toISOString(),
+      customerName: "Williams",
+      Amount: 794,
+    },
+  ];
+};
---
+++ invoice_list_mock_api/server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var cors = require("cors");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors({ origin: true }));
+
+var getInvoiceList = require("./data/getInvoiceList");
+
+app.get("/invoices", function (req, res) {
+  console.log("/invoices");
+  res.status(200).send(getInvoiceList());
+});
+
+app.listen(6001, () => {
+  console.log("Server started at 6001");
+});
