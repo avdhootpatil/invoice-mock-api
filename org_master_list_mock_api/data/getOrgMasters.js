72155461a165661abb97bddeb635e14462143dc4
Avdhoot Patil <avdhoot.980@gmail.com>
2020-10-29 19:39:45 +0530

    organiastion master list API initial setup

---
+++ org_master_list_mock_api/data/getOrgMasters.js
@@ -0,0 +1,34 @@
+module.exports = () => {
+  return [
+    {
+      id: 1,
+      name: "Astrum Labs",
+      alias: "astr",
+    },
+    {
+      id: 2,
+      name: "Facebook",
+      alias: "fb",
+    },
+    {
+      id: 3,
+      name: "Twitter",
+      alias: "tweet",
+    },
+    {
+      id: 4,
+      name: "Instagram",
+      alias: "insta",
+    },
+    {
+      id: 5,
+      name: "Snapchat",
+      alias: "snap",
+    },
+    {
+      id: 6,
+      name: "WhatsApp",
+      alias: "whats",
+    },
+  ];
+};
---
+++ org_master_list_mock_api/server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var cors = require("cors");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors({ origin: true }));
+
+var getOrgMasters = require("./data/getOrgMasters");
+
+app.get("/organisations", function (req, res) {
+  console.log("/organisations");
+  res.status(200).send(getOrgMasters());
+});
+
+app.listen(6001, () => {
+  console.log("Server started at 6001");
+});
