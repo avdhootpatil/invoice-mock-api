84090d371383fb725cd3aff6a7949c659e384383
Avdhoot Patil <avdhoot.980@gmail.com>
2020-11-24 13:10:07 +0530

    purchase list mock api update.

---
+++ purchase-list-mock-api/data/getPurchaseList.js
@@ -0,0 +1,53 @@
+module.exports = () => {
+  return [
+    {
+      id: 1,
+      purchaseRefNo: "a123",
+      prurchaseRefDate: new Date().toISOString(),
+      vendorName: "ferrari",
+      amount: 1000,
+    },
+    {
+      id: 2,
+      purchaseRefNo: "a234",
+      prurchaseRefDate: new Date().toISOString(),
+      vendorName: "Maclaren",
+      amount: 198,
+    },
+    {
+      id: 3,
+      purchaseRefNo: "a345",
+      prurchaseRefDate: new Date().toISOString(),
+      vendorName: "Mercedes",
+      amount: 100000,
+    },
+    {
+      id: 4,
+      purchaseRefNo: "a456",
+      prurchaseRefDate: new Date().toISOString(),
+      vendorName: "Renault",
+      amount: 100,
+    },
+    {
+      id: 5,
+      purchaseRefNo: "a567",
+      prurchaseRefDate: new Date().toISOString(),
+      vendorName: "Hass",
+      amount: 230,
+    },
+    {
+      id: 6,
+      purchaseRefNo: "a678",
+      prurchaseRefDate: new Date().toISOString(),
+      vendorName: "force india",
+      amount: 433,
+    },
+    {
+      id: 7,
+      purchaseRefNo: "a789",
+      prurchaseRefDate: new Date().toISOString(),
+      vendorName: "Williams",
+      amount: 794,
+    },
+  ];
+};
---
+++ purchase-list-mock-api/server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var cors = require("cors");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors({ origin: true }));
+
+var getPurchaseList = require("./data/getPurchaseList");
+
+app.get("/purchase", function (req, res) {
+  console.log("/purchase");
+  res.status(200).send(getPurchaseList());
+});
+
+app.delete("/purchase/:id", function (req, res) {
+  console.log("purchase deleted");
+  res.status(200).send("success");
+});
+
+app.listen(6005, () => {
+  console.log("Server started at 6005");
+});
